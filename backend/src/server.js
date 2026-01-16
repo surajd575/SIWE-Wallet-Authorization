500fad5854b439b71eeef13358bb14f082e706ab
Suraj Dhiman <dhimansuraj578@gmail.com>
2026-01-16 13:56:37 -0500

    Initial Commit

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express());
+
+app.get("/", (req, res) => {
+
+    res.json({ status: "Backend running"});
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
